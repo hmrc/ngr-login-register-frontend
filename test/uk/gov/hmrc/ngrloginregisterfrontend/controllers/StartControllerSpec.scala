c98d64a597b7cca1a12518fd801c06c4879fcb08
Paul <halpo@me.com>
2025-02-06 14:51:07 +0000

    NGR-552 Register start page (#8)
    * NGR-552 added classes
    
    * added components
    
    * added new page, bumped coverage back to 100%
    
    * made StartController a singleton, injected into the controller tests
    
    * removed back link

---
+++ test/uk/gov/hmrc/ngrloginregisterfrontend/controllers/StartControllerSpec.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ngrloginregisterfrontend.controllers
+
+import play.api.http.Status.OK
+import play.api.test.Helpers.{defaultAwaitTimeout, status}
+import uk.gov.hmrc.ngrloginregisterfrontend.helpers.ControllerSpecSupport
+import uk.gov.hmrc.ngrloginregisterfrontend.views.html.StartView
+
+class StartControllerSpec extends ControllerSpecSupport {
+  lazy val startView: StartView = inject[StartView]
+  lazy val controller: StartController = inject[StartController]
+
+  "Start Controller" must {
+    "return OK and the correct view for a GET" in {
+      val result = controller.show()(authenticatedFakeRequest)
+      status(result) mustBe OK
+    }
+
+    "redirect when start button pressed" in {
+      val result = controller.submit()(authenticatedFakeRequest)
+      status(result) mustBe 303
+    }
+  }
+}
