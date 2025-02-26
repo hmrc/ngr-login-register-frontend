8b70b672ad43c663b676ebf4d184457a8868c9ee
Jake Reid <75027964+JakeReid2020@users.noreply.github.com>
2025-02-26 11:54:35 +0000

    [NGR-687] Updated Regex (#25)
    * [NGR-687] Updated Regex
    
    * [NGR-687] capping name length and special characters also removing - from phone numbers
    
    * Adding linkid to ConfirmContactDetails

--- app/uk/gov/hmrc/ngrloginregisterfrontend/models/forms/CommonFormValidators.scala
+++ app/uk/gov/hmrc/ngrloginregisterfrontend/models/forms/CommonFormValidators.scala
@@ -21,9 +21,14 @@ import com.google.common.base.Strings
 import java.util.regex.Pattern
 
 trait CommonFormValidators {
-  val phoneNumberRegexPattern: Pattern  = Pattern.compile("^(\\+)?[0-9\\(\\)\\- ]{9,16}$")
+
+  val fullNameRegexPattern: Pattern     = Pattern.compile("^[A-Za-z .'-]{1,160}$")
+  val phoneNumberRegexPattern: Pattern  = Pattern.compile("^(\\+)?[0-9\\(\\)\\ ]{9,16}$")
+
   val isNonEmpty: String => Boolean = value => !Strings.isNullOrEmpty(value) && value.trim.nonEmpty
   val isMatchingPattern: (String, Pattern) => Boolean = (value, pattern) => pattern.matcher(value).matches()
+  val isValidFullName: String => Boolean = (value: String) =>
+    value.isEmpty || isMatchingPattern(value, fullNameRegexPattern)
   val isValidTelephoneNumber: String => Boolean = (value: String) =>
     value.isEmpty || isMatchingPattern(value, phoneNumberRegexPattern)
 }
