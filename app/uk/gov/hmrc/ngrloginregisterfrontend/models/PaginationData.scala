f79ac1f777c0a0d7c8336ac7de1a1dee7a0640a0
william-hatzar <22661741+william-hatzar@users.noreply.github.com>
2025-03-12 13:41:44 +0000

    Pagination wip

---
+++ app/uk/gov/hmrc/ngrloginregisterfrontend/models/PaginationData.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ngrloginregisterfrontend.models
+
+import uk.gov.hmrc.govukfrontend.views.html.components._
+import uk.gov.hmrc.govukfrontend.views.viewmodels.pagination.{Pagination, PaginationItem, PaginationLink}
+
+case class PaginationData(
+                           paginatedAddress: Option[PaginatedAddress],
+                           baseUrl: String,
+                         ) {
+  def toPagination: Pagination = {
+    val totalPages = paginatedAddress.map(_.totalPages).getOrElse(0)
+    val currentPage = paginatedAddress.map(_.currentPage).getOrElse(0)
+
+    val items = (1 to totalPages).map { pageNumber =>
+
+      PaginationItem(
+        href = s"$baseUrl/$pageNumber",
+        number = Some(pageNumber.toString),
+        current = if (pageNumber == currentPage) Some(true) else None
+      )
+    }
+
+    Pagination(
+      items = Some(items),
+      previous = if (currentPage > 1) Some(PaginationLink(href = s"$baseUrl/${currentPage - 1}")) else None,
+      next = if (currentPage < totalPages) Some(PaginationLink(href = s"$baseUrl/${currentPage + 1}")) else None
+    )
+  }
+}
+
+object PaginationHelper {
+  def createPagination(paginatedAddress: Option[PaginatedAddress], baseUrl: String): Pagination = {
+    PaginationData(paginatedAddress, baseUrl).toPagination
+  }
+}
