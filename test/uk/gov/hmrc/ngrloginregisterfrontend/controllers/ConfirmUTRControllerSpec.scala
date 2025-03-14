9f1ce33366838537cdf7a87176efb9466a4e8c4c
Paul <halpo@me.com>
2025-03-14 15:46:26 +0000

    added tests

---
+++ test/uk/gov/hmrc/ngrloginregisterfrontend/controllers/ConfirmUTRControllerSpec.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ngrloginregisterfrontend.controllers
+
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import play.api.http.Status._
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout, status}
+import uk.gov.hmrc.auth.core.Nino
+import uk.gov.hmrc.http.HeaderNames
+import uk.gov.hmrc.ngrloginregisterfrontend.connectors.CitizenDetailsConnector
+import uk.gov.hmrc.ngrloginregisterfrontend.helpers.ControllerSpecSupport
+import uk.gov.hmrc.ngrloginregisterfrontend.models.{AuthenticatedUserRequest, ErrorResponse, SaUtr}
+import uk.gov.hmrc.ngrloginregisterfrontend.models.cid.MatchingDetails
+import uk.gov.hmrc.ngrloginregisterfrontend.views.html.ConfirmUTRView
+
+import scala.concurrent.Future
+
+class ConfirmUTRControllerSpec extends ControllerSpecSupport {
+
+  val view: ConfirmUTRView = inject[ConfirmUTRView]
+  val mockCIDConnector: CitizenDetailsConnector = mock[CitizenDetailsConnector]
+  def controller =  new ConfirmUTRController(view, mockAuthJourney, mockCIDConnector, mcc)
+  val matchingDetails: MatchingDetails = MatchingDetails("bob", "jones", Some(SaUtr("1234567890")))
+  val matchingDetailsNoUTR: MatchingDetails = MatchingDetails("bob", "jones", None)
+  val pageTitle: String = "Confirm your Self Assessment Unique Taxpayer Reference"
+
+  "ConfirmUTRController" must {
+    "Return OK and the correct view" in {
+      when(mockCIDConnector.getMatchingResponse(any())(any()))
+        .thenReturn(Future.successful(Right(matchingDetails)))
+      val result = controller.show()(authenticatedFakeRequest)
+      status(result) mustBe OK
+      val content = contentAsString(result)
+      content must include(pageTitle)
+    }
+    "Return OK and the correct view no UTR" in {
+      when(mockCIDConnector.getMatchingResponse(any())(any()))
+        .thenReturn(Future.successful(Right(matchingDetailsNoUTR)))
+      val exception = intercept[RuntimeException] {
+        controller.show()(authenticatedFakeRequest).futureValue
+      }
+      exception.getMessage must include("No SAUTR found")
+    }
+    "error" in {
+      when(mockCIDConnector.getMatchingResponse(any())(any()))
+        .thenReturn(Future.successful(Left(ErrorResponse(404, "help"))))
+      val exception = intercept[RuntimeException] {
+        controller.show()(authenticatedFakeRequest).futureValue
+      }
+      exception.getMessage must include("call to citizen details failed: 404 help")
+    }
+    "method submit" must {
+//      "Successfully submit valid selection and redirect" in {
+//        val result = controller.submit()(AuthenticatedUserRequest(FakeRequest(routes.ConfirmUTRController.submit).withFormUrlEncodedBody(("ConfirmUTR", "yes")).withHeaders(HeaderNames.authorisation -> "Bearer 1"), None, None, None, None, None, None, nino = Nino(hasNino = true, Some(""))))
+//        status(result) mustBe SEE_OTHER
+//      }
+      "no selection" in {
+          val result = controller.submit()(AuthenticatedUserRequest(FakeRequest(routes.ConfirmUTRController.submit).withFormUrlEncodedBody(("ConfirmUTR", "")).withHeaders(HeaderNames.authorisation -> "Bearer 1"), None, None, None, None, None, None, nino = Nino(hasNino = true, Some(""))))
+          status(result) mustBe BAD_REQUEST
+          val content = contentAsString(result)
+          content must include(pageTitle)
+          content must include("Please select an option")
+      }
+    }
+  }
+}
---
+++ test/uk/gov/hmrc/ngrloginregisterfrontend/controllers/ConfirmUTRViewSpec.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ngrloginregisterfrontend.controllers
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import uk.gov.hmrc.ngrloginregisterfrontend.helpers.ViewBaseSpec
+import uk.gov.hmrc.ngrloginregisterfrontend.models.SaUtr
+import uk.gov.hmrc.ngrloginregisterfrontend.models.registration.ConfirmUTR
+import uk.gov.hmrc.ngrloginregisterfrontend.views.html.ConfirmUTRView
+
+class ConfirmUTRViewSpec extends ViewBaseSpec {
+  lazy val view: ConfirmUTRView = inject[ConfirmUTRView]
+  lazy val controller: ConfirmUTRController = inject[ConfirmUTRController]
+
+  val hint: String = "Register for the business rates valuation service"
+  val title: String = "Confirm your Self Assessment Unique Taxpayer Reference"
+  val body: String = "We will display the last 3 digits of your Unique Taxpayer Reference (UTR). You can provide this UTR to join up the accounts you use to pay tax."
+  val sautr: String = "Self Assessment Unique Taxpayer Reference"
+  val yes: String = "Yes, I want to provide this UTR"
+  val noNI: String = "No, I want to provide my National Insurance number"
+  val noLater: String = "No, I will provide a tax reference number later"
+  val errorMessage: String = "Please select an option"
+
+  object Selectors {
+    val hint: String = "#main-content > div > div > form > span"
+    val title: String = "#main-content > div > div > form > h1"
+    val body: String = "#main-content > div > div > form > p"
+    val sautr: String = "#main-content > div > div > form > dl > div > dt"
+    val yes: String = "#main-content > div > div > form > div > div > div:nth-child(1) > label"
+    val noNI: String = "#main-content > div > div > form > div > div > div:nth-child(2) > label"
+    val noLater: String = "#main-content > div > div > form > div > div > div:nth-child(3) > label"
+    val errorMessage: String = "#main-content > div > div > form > div.govuk-error-summary > div > div > ul > li > a"
+  }
+
+  "ConfirmUTRView" must {
+    "produce the same output for apply() and render()" in {
+      val form = ConfirmUTR
+        .form()
+
+      val utr: SaUtr = SaUtr("1234567890")
+
+      val summaryList = controller.summaryList(utr.value)
+
+      val radios = controller.radios()
+
+      val htmlApply = view.apply(form, summaryList, radios).body
+
+      val htmlRender = view.render(form, summaryList, radios, request, messages, mockConfig).body
+
+      val htmlF = view.f(form, summaryList, radios)(request, messages, mockConfig).body
+
+      htmlApply mustBe htmlRender
+      htmlF must not be empty
+      lazy implicit val document: Document = Jsoup.parse(view(form, summaryList, radios)(request, messages, mockConfig).body)
+      elementText(Selectors.hint) mustBe hint
+      elementText(Selectors.title) mustBe title
+      elementText(Selectors.body) mustBe body
+      elementText(Selectors.sautr) mustBe sautr
+      elementText(Selectors.yes) mustBe yes
+      elementText(Selectors.noNI) mustBe noNI
+      elementText(Selectors.noLater) mustBe noLater
+    }
+  }
+
+}
