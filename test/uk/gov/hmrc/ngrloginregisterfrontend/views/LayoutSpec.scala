d28f8d475847f6d121252a16874757d512169866
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2025-03-17 11:09:05 +0000

    [Layout-Changes] Making pages two-thirds and increasing input text size for Name and Email

--- test/uk/gov/hmrc/ngrloginregisterfrontend/views/LayoutSpec.scala
+++ test/uk/gov/hmrc/ngrloginregisterfrontend/views/LayoutSpec.scala
@@ -44,8 +44,8 @@ class LayoutSpec extends ViewBaseSpec {
 
     "produce the same output for apply() and render()" in {
       val htmlApply = injectedView.apply(pageTitle = Some("Title of page"), showBackLink = false)(Html("Test")).body
-      val htmlRender = injectedView.render(pageTitle = Some("Title of page"), showBackLink = false, contentBlock = Html("Test"), request = request, messages = messages, appConfig = mockConfig).body
-      val htmlF = injectedView.f(Some("Title of page"), false)(Html("Test"))(request, messages, mockConfig).body
+      val htmlRender = injectedView.render(pageTitle = Some("Title of page"), showBackLink = false, contentBlock = Html("Test"), request = request, messages = messages, appConfig = mockConfig, fullWidth = false).body
+      val htmlF = injectedView.f(Some("Title of page"), false, false)(Html("Test"))(request, messages, mockConfig).body
       htmlApply mustBe htmlRender
       htmlF must not be empty
     }
