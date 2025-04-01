f2b080da614d15edda01d5292518fdb11e927879
anna-shen-hmrc <anna-shen-hmrc@users.noreply.github.com>
2025-04-01 14:46:26 +0100

    address PR-bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"                        % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"                        % "11.12.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"                        % "11.13.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                                % hmrcMongoVersion,
     "com.beachape"            %% "enumeratum-play-json"                              % "1.8.2",
     "uk.gov.hmrc"             %% "domain-play-30"                                    % "10.0.0",
