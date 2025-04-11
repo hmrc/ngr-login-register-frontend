187202c329f451ed30dd01ab563f0ef3fa1b0434
anna-shen-hmrc <anna-shen-hmrc@users.noreply.github.com>
2025-04-11 10:10:50 +0100

    NGR-1293: adding tests

--- app/uk/gov/hmrc/ngrloginregisterfrontend/controllers/ConfirmAddressController.scala
+++ app/uk/gov/hmrc/ngrloginregisterfrontend/controllers/ConfirmAddressController.scala
@@ -18,7 +18,7 @@ package uk.gov.hmrc.ngrloginregisterfrontend.controllers
 
 import play.api.i18n.I18nSupport
 import play.api.libs.json.Json
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Session}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Result}
 import uk.gov.hmrc.ngrloginregisterfrontend.config.AppConfig
 import uk.gov.hmrc.ngrloginregisterfrontend.connectors.NGRConnector
 import uk.gov.hmrc.ngrloginregisterfrontend.controllers.auth.AuthJourney
@@ -28,6 +28,7 @@ import uk.gov.hmrc.ngrloginregisterfrontend.models.forms.Address
 import uk.gov.hmrc.ngrloginregisterfrontend.models.forms.ConfirmAddressForm.form
 import uk.gov.hmrc.ngrloginregisterfrontend.models.registration.CredId
 import uk.gov.hmrc.ngrloginregisterfrontend.session.SessionManager
+import uk.gov.hmrc.ngrloginregisterfrontend.utils.SessionTimeoutHelper
 import uk.gov.hmrc.ngrloginregisterfrontend.views.html.ConfirmAddressView
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
@@ -40,49 +41,58 @@ class ConfirmAddressController @Inject()(confirmAddressView: ConfirmAddressView,
                                          sessionManager: SessionManager,
                                          connector: NGRConnector,
                                          mcc: MessagesControllerComponents)(implicit appConfig: AppConfig, ec: ExecutionContext)
-  extends FrontendController(mcc) with I18nSupport {
+  extends FrontendController(mcc) with I18nSupport with SessionTimeoutHelper {
   private val yesButton: NGRRadioButtons = NGRRadioButtons("Yes", Yes)
   private val noButton: NGRRadioButtons = NGRRadioButtons("No", No)
   private val ngrRadio: NGRRadio = NGRRadio(NGRRadioName("confirm-address-radio"), Seq(yesButton, noButton))
   def show(mode: String): Action[AnyContent] =
     authenticate.authWithUserDetails.async { implicit request =>
-      Future.successful(Ok(confirmAddressView(getAddressFromSession(request.session), form, buildRadios(form, ngrRadio), mode)))
+      getAddressFromSession match {
+        case Right(addressOpt) => Future.successful(Ok(confirmAddressView(getChosenAddressString(addressOpt), form, buildRadios(form, ngrRadio), mode)))
+        case Left(result) => Future.successful(result)
+      }
     }
 
   def submit(mode: String): Action[AnyContent] =
     authenticate.authWithUserDetails.async { implicit request =>
+      def redirectPage(mode: String): Result = if (mode == "CYA") Redirect(routes.CheckYourAnswersController.show) else Redirect(routes.ConfirmContactDetailsController.show)
       form
         .bindFromRequest()
         .fold(
           formWithErrors =>
-            Future.successful(BadRequest(confirmAddressView(getAddressFromSession(request.session), formWithErrors, buildRadios(formWithErrors, ngrRadio), mode))),
+            getAddressFromSession match {
+              case Right(addressOpt) => Future.successful(BadRequest(confirmAddressView(getChosenAddressString(addressOpt), formWithErrors, buildRadios(formWithErrors, ngrRadio), mode)))
+              case Left(result) => Future.successful(result)
+            },
           confirmAddressForm => {
-            updateAddress(confirmAddressForm.radioValue).map { _ =>
-              if (mode == "CYA") {
-                Redirect(routes.CheckYourAnswersController.show)
-              } else {
-                Redirect(routes.ConfirmContactDetailsController.show)
-              }
+            val result:Result = if (confirmAddressForm.radioValue.equals("Yes")) {
+              updateAddress().fold(result => result, _ => redirectPage(mode))
+            } else {
+              redirectPage(mode)
             }
+            Future.successful(result)
           }
         )
     }
 
-  private def updateAddress(value: String)(implicit request: AuthenticatedUserRequest[_]): Future[Unit] =
-    if (value == "Yes") {
-      sessionManager.getSessionValue(request.session, sessionManager.chosenAddressIdKey)
-        .map(Json.parse(_).as[Address])
-        .map(connector.changeAddress(CredId(request.credId.getOrElse("")), _).map(_ => ()))
-        .getOrElse(Future.unit)
-    } else {
-      Future.unit
-    }
+  private def updateAddress()(implicit request: AuthenticatedUserRequest[_]): Either[Result, Unit] =
+    getAddressFromSession match {
+        case Right(addressOpt) =>
+          addressOpt
+            .map(connector.changeAddress(CredId(request.credId.getOrElse("")), _))
+            .map(_ => Right())
+            .getOrElse(Right())
+        case Left(result) => Left(result)
+      }
 
-
-  private def getAddressFromSession(session: Session): String =
-    sessionManager.getSessionValue(session, sessionManager.chosenAddressIdKey)
-    .map(Json.parse(_).as[Address])
-    .map(address => s"${address.line1}, ${if (address.line2.isDefined) address.line2.get + "," else ""} ${address.town} ${address.postcode}")
+  private def getChosenAddressString(addressOpt: Option[Address]): String =
+    addressOpt
+    .map(address => s"${address.line1}, ${address.line2.map(line2 => s"$line2,").getOrElse("")} ${address.town} ${address.postcode}")
     .getOrElse("")
+
+  private def getAddressFromSession()(implicit request: AuthenticatedUserRequest[_]): Either[Result, Option[Address]] =
+    getSession(sessionManager, request.session, sessionManager.chosenAddressIdKey)
+      .fold(result => Left(result), addressStrOpt => Right(addressStrOpt.map(Json.parse(_).as[Address])))
+
 
 }
