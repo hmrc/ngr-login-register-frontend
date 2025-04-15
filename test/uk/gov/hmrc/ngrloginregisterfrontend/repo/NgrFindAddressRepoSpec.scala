2b701039ff823514d3c6dd50c809057add614555
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2025-04-15 11:11:29 +0100

    [NGR-1298] Adding Extra Tests

--- test/uk/gov/hmrc/ngrloginregisterfrontend/repo/NgrFindAddressRepoSpec.scala
+++ test/uk/gov/hmrc/ngrloginregisterfrontend/repo/NgrFindAddressRepoSpec.scala
@@ -17,6 +17,7 @@
 package uk.gov.hmrc.ngrloginregisterfrontend.repo
 
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import play.api.test.Helpers.await
 import uk.gov.hmrc.mongo.test.DefaultPlayMongoRepositorySupport
 import uk.gov.hmrc.ngrloginregisterfrontend.helpers.TestSupport
 import uk.gov.hmrc.ngrloginregisterfrontend.models.Postcode
@@ -63,7 +64,23 @@ class NgrFindAddressRepoSpec extends TestSupport
         exception.getMessage contains "Addresses have not been inserted" shouldBe true
       }
     }
+
+    "find a ratepayer by cred id" when {
+      "correct ratepayer has been returned" in {
+        await(repository.upsertLookupAddresses(lookUpAddresses3))
+        val isSuccessful = await(repository.findByCredId(credId))
+
+        val respoonse = isSuccessful.copy(createdAt = time)
+        val expected = lookUpAddresses3.copy(createdAt = time)
+        respoonse shouldBe expected
+      }
+    }
+
+    "credId doesn't exist in mongoDB" in {
+      val actual = await(repository.findByCredId(credId))
+       val response =  actual.copy(createdAt = time)
+
+      response shouldBe LookUpAddresses(credId = credId, createdAt = time, postcode = Postcode(""))
+    }
   }
-
-
 }
