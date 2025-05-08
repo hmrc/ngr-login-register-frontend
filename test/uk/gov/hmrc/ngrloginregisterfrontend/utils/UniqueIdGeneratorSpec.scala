74131b2cbb389fdca11c25d8ef721eb0011a20bc
Paul <halpo@me.com>
2025-05-08 08:53:08 +0100

    improved tests

--- test/uk/gov/hmrc/ngrloginregisterfrontend/utils/UniqueIdGeneratorSpec.scala
+++ test/uk/gov/hmrc/ngrloginregisterfrontend/utils/UniqueIdGeneratorSpec.scala
@@ -7,44 +7,42 @@ class UniqueIdGeneratorSpec extends TestSupport {
   private val allowedChars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"
 
   "UniqueIdGenerator" must {
+
     "generate a 12 char id with 2 hyphens" in {
       val id = UniqueIdGenerator.generateId
       id.length mustBe 14
-      id.split("-").mkString.length mustBe 12
-      id.split("-").mkString.forall(allowedChars.contains(_)) mustBe true
+      val compactId = id.replace("-", "")
+      compactId.length mustBe 12
+      compactId.forall(allowedChars.contains(_)) mustBe true
     }
-    "validate an input id" in {
-      UniqueIdGenerator.validateId("0FDE-DFD1-DGJ1") match {
-        case Left(error) => succeed
-        case Right(value) => fail("should be an error")
+
+    "invalidate bad IDs" in {
+      val invalidIds = List(
+        "0FDE-DFD1-DGJ1",
+        "0efkdkfvncma",
+        "hello",
+        "&fdh-9adf-4jnf"
+      )
+
+      invalidIds.foreach { id =>
+        withClue(s"Expected '$id' to be invalid: ") {
+          UniqueIdGenerator.validateId(id).isLeft mustBe true
+        }
       }
-      UniqueIdGenerator.validateId("0efkdkfvncma") match {
-        case Left(error) => succeed
-        case Right(value) => fail("should be an error")
-      }
-      UniqueIdGenerator.validateId("hello") match {
-        case Left(error) => succeed
-        case Right(value) => fail("should be an error")
-      }
-      UniqueIdGenerator.validateId("&fdh-9adf-4jnf") match {
-        case Left(error) => succeed
-        case Right(value) => fail("should be an error")
-      }
-      UniqueIdGenerator.validateId("fdfd-fdfd-dfdf") match {
-        case Left(error) => fail("should be valid")
-        case Right(value) => succeed
-      }
-      UniqueIdGenerator.validateId("VDJ4-5NSG-8RHW") match {
-        case Left(error) => fail("should be valid")
-        case Right(value) => succeed
-      }
-      UniqueIdGenerator.validateId("BDJ6867MLMNE") match {
-        case Left(error) => fail("should be valid")
-        case Right(value) => succeed
-      }
-      UniqueIdGenerator.validateId("nvjf5245bsmv") match {
-        case Left(error) => fail("should be valid")
-        case Right(value) => succeed
+    }
+
+    "validate good IDs" in {
+      val validIds = List(
+        "fdfd-fdfd-dfdf",
+        "VDJ4-5NSG-8RHW",
+        "BDJ6867MLMNE",
+        "nvjf5245bsmv"
+      )
+
+      validIds.foreach { id =>
+        withClue(s"Expected '$id' to be valid: ") {
+          UniqueIdGenerator.validateId(id).isRight mustBe true
+        }
       }
     }
   }
